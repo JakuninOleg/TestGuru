eea0cefe597cf61259815c530dec01325adb31ca
Oleg Jakunin <oleg.kemz@gmail.com>
2018-02-21 05:29:31 +0500

    done

---
+++ db/migrate/20180220234619_create_answers.rb
@@ -0,0 +1,12 @@
+class CreateAnswers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :answers do |t|
+      t.string :body
+      t.boolean :correct
+      t.integer :user_id
+      t.integer :question_id
+
+      t.timestamps
+    end
+  end
+end
