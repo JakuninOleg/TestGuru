382db8c3e886252409ba9686dcfa64e7aca4a4eb
Oleg Jakunin <oleg.kemz@gmail.com>
2018-02-22 06:14:24 +0500

    done

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,5 @@
 class Test < ApplicationRecord
+  def self.sorted_tests_array(category)
+    Test.select('tests.title, tests.id').joins('JOIN categories ON tests.category_id = categories.id').where('categories.name = ?', category).order('tests.title DESC')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def show(difficulty)
+    Test.select('tests.title, tests.id').joins('JOIN test_passages ON tests.id = test_passages.test_id').joins('JOIN users ON test_passages.user_id = users.id').where('tests.level = ? AND test_passages.user_id = ?', difficulty, id)
+  end
 end
