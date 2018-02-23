bd1397752781364100a40c0c369085b2eaee5b27
Oleg Jakunin <oleg.kemz@gmail.com>
2018-02-24 00:00:04 +0500

    fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,6 @@
 class Test < ApplicationRecord
-  def self.sorted_tests_array(category)
-    tests = Test.select('tests.title').
-    joins('JOIN categories ON tests.category_id = categories.id').
-    where('categories.name = ?', category).order('tests.title DESC').map(&:title)
+  def self.sorted_category_array(category)
+    Test.joins('JOIN categories ON tests.category_id = categories.id').
+    where(categories: { name: category }).order('tests.title DESC').pluck('title')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  def show(difficulty)
-    Test.select('tests.title, tests.id').
-    joins('JOIN test_passages ON tests.id = test_passages.test_id').
-    where('tests.level = ? AND test_passages.user_id = ?', difficulty, id)
+  def show_passed_tests(difficulty)
+    Test.joins('JOIN test_passages ON tests.id = test_passages.test_id').
+    where(level: difficulty, test_passages: { user_id: id })
   end
 end
