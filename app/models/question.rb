7d6eef4e5ad1540ce0e561254d8699146cfadc9f
Oleg Jakunin <oleg.kemz@gmail.com>
2018-02-24 03:47:57 +0500

    initial associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  has_many :answers
+
+  belongs_to :test
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :test_passages
+  has_many :tests, through: :test_passages
+
   def show_passed_tests(difficulty)
     Test.joins('JOIN test_passages ON tests.id = test_passages.test_id').
     where(level: difficulty, test_passages: { user_id: id })
