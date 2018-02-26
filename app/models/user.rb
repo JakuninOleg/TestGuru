fcb6ecff0a6b72cbc995d9079b58628ce039310b
Oleg Jakunin <oleg.kemz@gmail.com>
2018-02-26 05:35:51 +0500

    Missprint fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  scope :passed_tests, -> (user, difficulty) { Test.quijoins(:test_passages).
+  scope :passed_tests, -> (user, difficulty) { Test.joins(:test_passages).
     where(level: difficulty, test_passages: { user_id: user.id }) }
 
     validates :name, presence: true
