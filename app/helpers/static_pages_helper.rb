2fe6a6ff89acf7392915a136df0ed9695832619f
Oleg Jakunin <oleg.kemz@gmail.com>
2018-03-06 03:05:32 +0500

    Views, helpers are done

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+  def current_year
+    Time.current.year
+  end
+
+  def github_url(author, repo)
+    link_to 'Test Guru', "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
