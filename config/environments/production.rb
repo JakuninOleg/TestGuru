fc5fa5260346f5ff0ecc86c170691f220604f19b
Oleg Jakunin <oleg.kemz@gmail.com>
2018-04-08 03:39:14 +0500

    new commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,15 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: '.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'secure-refuge-53397
+.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:             'smtp.gmail.com',
     port:                 587,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
-    authentication:       plain,
+    authentication:       'plain',
     enable_starttls_auto: true }
 
 
