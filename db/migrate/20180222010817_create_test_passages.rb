a1d5a30ef71f9dacdf4de5c27ba6ae2a58167096
Oleg Jakunin <oleg.kemz@gmail.com>
2018-04-09 04:08:03 +0500

    fix

--- db/migrate/20180222010817_create_test_passages.rb
+++ db/migrate/20180222010817_create_test_passages.rb
@@ -3,6 +3,7 @@ class CreateTestPassages < ActiveRecord::Migration[5.1]
     create_table :test_passages do |t|
       t.integer :user_id, foreign_key: true
       t.integer :test_id, foreign_key: true
+      t.references :current_question, foreign_key: { to_table: :questions }
 
       t.timestamps
     end
--- db/migrate/20180310232750_add_currentquestion_to_testpassages.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrentquestionToTestpassages < ActiveRecord::Migration[5.1]
-  def change
-    add_reference :test_passages, :current_question, foreign_key: true
-  end
-end
